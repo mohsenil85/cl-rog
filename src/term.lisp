3cd1feada64d6c4cb3c42984abcbf4425fcc9815
Logan Mohseni <mohsenil85@gmail.com>
2014-10-13 17:42:09 -0700

    refactorings

--- src/term.lisp
+++ src/term.lisp
@@ -6,63 +6,55 @@
 (in-package :term)
 
 
-;
-;(dotimes '10
-;  (progn
-;  (sleep (random .3))
-;  (princ "foo")))
-;  
 
 (defstruct player x y hp)
+
+(defparameter *player* (make-player  :x 20 :y 20 :hp 20))
 
 (defun move (player dir)
   (format t "~A~A~%" player dir))
 
-(defun erase-old ()
+
+(defun paint (ch)
   (with-restored-cursor *standard-window*
-    (write-char-at-cursor *standard-window* #\Space)))
-(defun paint ()
-  (with-restored-cursor *standard-window*
-    (write-char-at-cursor *standard-window* #\@)))
-;(let ((p (make-player :x 10 :y  7 :hp 10))) 
-  ;(move p :up))
+    (write-char-at-cursor *standard-window* ch)))
 
 (defun main ()
   (with-curses  ()
     (disable-echoing)
+    (cl-charms/low-level:curs-set 0)
+
     (enable-raw-input :interpret-control-characters t)
     (enable-non-blocking-mode *standard-window*)
 
-    (let ((p (make-player :x 10 :y 7 :hp 10)))
+    ;(let ((p (make-player :x 10 :y 7 :hp 10)))
 
-    ;(multiple-value-bind (width height)
-      ;(window-dimensions *standard-window*)
 
       (progn
         (loop :named main-loop
-              :for c := (get-char *standard-window* :ignore-error t) 
-              :with x := (player-x p)
-              :with y := (player-y p)
-
               :do (progn
-                    (refresh-window *standard-window*)
-                    (erase-old)
-                    (case c
-                      ((nil) nil)
-                      ((#\k) (decf y))
-                      ((#\j) (incf y))
-                      ((#\h) (decf x))
-                      ((#\l) (incf x))
 
-                      ((#\q) (return-from main-loop)))
+                    (refresh-window *standard-window*)
+                    (paint #\Space)
+
+                    (let ((c (get-char *standard-window* :ignore-error t) )) 
+                      (case c
+                      ((nil) nil)
+                      ((#\k) (decf (player-y *player*  )))
+                      ((#\j) (incf (player-y *player*  )))
+                      ((#\h) (decf (player-x *player*  )))
+                      ((#\l) (incf (player-x *player*  )))
+
+                      ((#\q) (return-from main-loop))))
                     
-                    (move-cursor *standard-window* x y)
-                    (paint)
+                    (move-cursor *standard-window* 
+                                 (player-x *player*) 
+                                 (player-y *player*))
+                    (paint #\@)
                     
                     
                     )
               
-              )
-        ))));)
+              ))));)
 
 (main)
