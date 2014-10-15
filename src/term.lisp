614fe129377f1eac733a50ea908025532f53c6a0
Logan Mohseni <logan.mohseni@singlemindconsulting.com>
2014-10-15 14:23:22 -0700

    added with-color macro

--- src/term.lisp
+++ src/term.lisp
@@ -19,6 +19,9 @@
 (defparameter *world-age* 0)
 (defparameter *plants* '() )
 
+(defparameter red 1)
+(defparameter blue 2)
+(defparameter green 3)
 
 (defun init ()
   (disable-echoing)
@@ -26,6 +29,9 @@
   (enable-raw-input :interpret-control-characters t)
   (enable-non-blocking-mode *standard-window*)
   (cl-charms/low-level:start-color)
+  (cl-charms/low-level:init-pair red cl-charms/low-level:color_red charms/ll:color_black )
+  (cl-charms/low-level:init-pair blue cl-charms/low-level:color_blue charms/ll:color_black )
+  (cl-charms/low-level:init-pair green cl-charms/low-level:color_green charms/ll:color_black )
   )
 
 (defun paint (ch)
@@ -142,15 +148,19 @@
         :do
          (draw-plant p))))
 
+(defmacro with-color (color &body body)
+  `(progn
+    (cl-charms/low-level:attron (cl-charms/low-level:color-pair ,color))  
+    ,@body
+  (cl-charms/low-level:attroff (cl-charms/low-level:color-pair ,color)))  )
 
 (defun draw-player ()
-    (if (char-equal #\@ 
-                    (char-at-cursor *standard-window* )) 
-      (paint #\Space))
-    (move-cursor *standard-window* 
-                 (player-x *player*) 
-                 (player-y *player*))
-    (paint #\@))
+  (paint #\Space)
+  (move-cursor *standard-window* 
+               (player-x *player*) 
+               (player-y *player*))
+  (with-color red (paint #\@))
+    )
 
 (defun draw-hud ()
   (string-point (write-to-string *world-age*) 0 0 ))
@@ -177,8 +187,8 @@
 (defun update-world ()
   (progn
     (incf *world-age*  )
-    (if (eq 0 (mod *world-age* 61)) 
-      (draw-map))
+    ;(if (eq 0 (mod *world-age* 61)) 
+    ;  (draw-map))
     (draw-hud)
     (draw-plants)
     (draw-player)
